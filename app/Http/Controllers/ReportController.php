48631edb20641ece0bfae72a1877c3209c35de40
ismaryanto <kotokareh@gmail.com>
2020-10-16 20:53:16 +0700

    asda

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -6,12 +6,7 @@ use App\TmbarangModel;
 use Illuminate\Http\Request;
 
 class ReportController extends Controller
-{
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+{ 
     public function index()
     {
 
